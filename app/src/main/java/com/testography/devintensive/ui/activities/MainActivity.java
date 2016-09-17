adfc466a35856282820bf1abf44c9305c29ef6fe
Leo Burdianov <leo.burdianov@gmail.com>
2016-09-17 22:57:04 +0400

    Add Material Design

--- app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
@@ -1,12 +1,9 @@
 package com.testography.devintensive.ui.activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.testography.devintensive.R;
 import com.testography.devintensive.utils.ConstantManager;
@@ -15,10 +12,6 @@ public class MainActivity extends AppCompatActivity implements View
         .OnClickListener {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
-
-    protected EditText mEditText;
-    protected Button mRedButton, mGreenButton;
-    protected int mColorMode;
 
     /**
      * @param savedInstanceState
@@ -29,20 +22,11 @@ public class MainActivity extends AppCompatActivity implements View
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
 
-        mRedButton = (Button) findViewById(R.id.red_btn);
-        mGreenButton = (Button) findViewById(R.id.green_btn);
-        mEditText = (EditText) findViewById(R.id.text_edit);
-
-        mRedButton.setOnClickListener(this);
-        mGreenButton.setOnClickListener(this);
-
         if (savedInstanceState == null) {
             // creating the activity for the first time
         } else {
-            mColorMode = savedInstanceState.getInt(ConstantManager.COLOR_MODE_KEY);
-        }
 
-        mEditText.setBackgroundColor(mColorMode);
+        }
     }
 
     @Override
@@ -84,22 +68,12 @@ public class MainActivity extends AppCompatActivity implements View
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.green_btn:
-                mEditText.setBackgroundColor(Color.GREEN);
-                mColorMode = Color.GREEN;
-                break;
-            case R.id.red_btn:
-                mEditText.setBackgroundColor(Color.RED);
-                mColorMode = Color.RED;
-                break;
+
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
-        Log.d(TAG, "onSaveInstanceState");
-        outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode);
     }
 }
