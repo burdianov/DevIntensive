7e65984999fa4b1a490f53acabf8212dce075f76
Leo Burdianov <leo.burdianov@gmail.com>
2016-09-18 22:54:56 +0400

    Create MainActivity base class containing the necessary common methods for all activities

--- app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
@@ -1,18 +1,20 @@
 package com.testography.devintensive.ui.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.testography.devintensive.R;
 import com.testography.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View
+public class MainActivity extends BaseActivity implements View
         .OnClickListener {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
 
+    private ImageView mCallImg;
     /**
      * @param savedInstanceState
      */
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
 
         if (savedInstanceState == null) {
             // creating the activity for the first time
@@ -68,12 +73,25 @@ public class MainActivity extends AppCompatActivity implements View
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
+            case R.id.call_img:
+                showProgress();
+                runWithDelay();
+                break;
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    private void runWithDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        }, 5000);
     }
 }
