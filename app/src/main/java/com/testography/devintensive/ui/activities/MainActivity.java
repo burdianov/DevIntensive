3e5fb1acae2a1f0ae6dfe05df986b052f348fcb9
Leo Burdianov <leo.burdianov@gmail.com>
2016-09-20 21:53:47 +0400

    Set Nav Drawer items onClick Listener

--- app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/testography/devintensive/ui/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.testography.devintensive.ui.activities;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -41,6 +43,7 @@ public class MainActivity extends BaseActivity implements View
         mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
 
         setupToolbar();
+        setupDrawer();
 
         mCallImg.setOnClickListener(this);
 
@@ -59,42 +62,6 @@ public class MainActivity extends BaseActivity implements View
             mNavigationDrawer.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
     }
 
     @Override
@@ -123,6 +90,21 @@ public class MainActivity extends BaseActivity implements View
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id
+                .navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
     }
 
     private void runWithDelay() {
