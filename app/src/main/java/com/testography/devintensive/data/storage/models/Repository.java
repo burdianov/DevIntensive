0ca850ec7bf48b5c3b38886a781f37e3738886f0
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-10 22:18:28 +0400

    Create the entities User and Repository

--- app/src/main/java/com/testography/devintensive/data/storage/models/Repository.java
+++ app/src/main/java/com/testography/devintensive/data/storage/models/Repository.java
@@ -1,5 +1,21 @@
 package com.testography.devintensive.data.storage.models;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.Unique;
+
+@Entity(active = true, nameInDb = "REPOSITORIES")
 public class Repository {
 
+    @Id
+    private Long id;
+
+    @NotNull
+    @Unique
+    private String remoteId;
+
+    private String repositoryName;
+
+    private String userRemoteId;
 }
--- app/src/main/java/com/testography/devintensive/data/storage/models/User.java
+++ app/src/main/java/com/testography/devintensive/data/storage/models/User.java
@@ -1,5 +1,44 @@
 package com.testography.devintensive.data.storage.models;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.JoinProperty;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.ToMany;
+import org.greenrobot.greendao.annotation.Unique;
+
+import java.util.List;
+
+@Entity(active = true, nameInDb = "USERS")
 public class User {
 
+    @Id
+    private Long id;
+
+    @NotNull
+    @Unique
+    private String remoteId;
+
+    private String photo;
+
+    @NotNull
+    @Unique
+    private String fullName;
+
+    @NotNull
+    @Unique
+    private String searchName;
+
+    private int rating;
+
+    private int codeLines;
+
+    private int projects;
+
+    private String bio;
+
+    @ToMany(joinProperties = {
+            @JoinProperty(name = "remoteId", referencedName = "userRemoteId")
+    })
+    private List<Repository> repositories;
 }
